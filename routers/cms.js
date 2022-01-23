7ee3d0d1fb0d1ad0960dcfdd56b88f0d72066354
CarlosJaramilloDev <carlosjaramillodev@gmail.com>
2022-01-23 13:56:39 -0500

    Change basic initial methods for Published and Draft methods

--- routers/cms.js
+++ routers/cms.js
@@ -5,7 +5,7 @@ const router = new express.Router();
 const hubspotClient = new hubspot.Client({ "apiKey" : "4c6dcb6d-d46b-48d8-a26b-5677fc77c29c" });
 const tableName = "developer_test";
 
-// Get users
+// Get publish table rows
 router.get("/users", async (req, res) => {
   try {
     const apiResponse = await hubspotClient.cms.hubdb.rowsApi.getTableRows(tableName)
@@ -17,7 +17,21 @@ router.get("/users", async (req, res) => {
   }
 });
 
-// Get user by id
+
+// Get draft table rows
+router.get("/users/draft", async (req, res) => {
+  try {
+    const apiResponse = await hubspotClient.cms.hubdb.rowsApi.readDraftTableRows(tableName)
+    res.send({data: apiResponse.body});
+  } catch (e) {
+    e.message === 'HTTP request failed'
+    ? res.status(e.response.statusCode).send({ message: e.response.body.message })
+    : res.status(500).send({ message: e.message })
+  }
+});
+
+
+// Get publish user by id
 router.get("/users/:id", async (req, res) => {
   try {
     const apiResponse = await hubspotClient.cms.hubdb.rowsApi.getTableRow(tableName, req.params.id)    
@@ -28,5 +42,41 @@ router.get("/users/:id", async (req, res) => {
     : res.status(500).send({ message: e.message })
   }
 });
+
+
+// Get draft user by id
+router.get("/users/draft/:id", async (req, res) => {
+  try {
+    const apiResponse = await hubspotClient.cms.hubdb.rowsApi.getDraftTableRowById(tableName, req.params.id)    
+    res.send({data: apiResponse.body});
+  } catch (e) {
+    e.message === 'HTTP request failed'
+    ? res.status(e.response.statusCode).send({ message: e.response.body.message })
+    : res.status(500).send({ message: e.message })
+  }
+});
+
+// Add new user to draft table
+router.post("/users", async (req, res) => {
+  try {
+    if(req.body.name && req.body.last_name && req.body.document_id){
+      const values = {
+        "name": req.body.name,
+        "last_name": req.body.last_name,
+        "document_id": req.body.document_id,
+      };
+      const apiResponse = await hubspotClient.cms.hubdb.rowsApi.createTableRow(tableName, { values })   
+      res.send({data: apiResponse.body});
+    } else {
+      res.status(400).send({ message: 'Name, Last name and Document ID are required' })
+    }
+  } catch (e) {
+    e.message === 'HTTP request failed'
+    ? res.status(e.response.statusCode).send({ message: e.response.body.message })
+    : res.status(500).send({ message: e.message })
+  }
+});
+  
+
 
 module.exports = router;
