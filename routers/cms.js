6a0882ec9efc5ae15a915f6fe95edbdea42e23a1
Carlos Jaramillo Dev <dpercepcion@gmail.com>
2022-01-23 11:54:00 -0500

    Added @hubspot/api-client and created get users method

--- routers/cms.js
+++ routers/cms.js
@@ -1,4 +1,20 @@
 const express = require("express");
+const hubspot = require('@hubspot/api-client');
 const router = new express.Router();
+
+const hubspotClient = new hubspot.Client({ "apiKey" : "4c6dcb6d-d46b-48d8-a26b-5677fc77c29c" });
+const tableName = "developer_test";
+
+// Get users
+router.get("/users", async (req, res) => {
+  try {
+    const apiResponse = await hubspotClient.cms.hubdb.rowsApi.getTableRows(tableName)
+    res.send({data: apiResponse.body});
+  } catch (e) {
+    e.message === 'HTTP request failed'
+    ? res.status(424).send({ message: e.response })
+    : res.status(500).send({ message: e })
+  }
+});
 
 module.exports = router;
